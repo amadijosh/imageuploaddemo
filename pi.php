31f3dd8a86544764699441d7e16a34ce2dbd4452
Aj Dev <ahero@heroku.com>
2015-07-27 18:46:02 +0100

    Image upload works!

--- pi.php
+++ pi.php
@@ -1,7 +1,7 @@
 <?php 
  echo "<pre />";
  //var_dump($_FILES['data']);
- var_dump($_POST);
+ var_dump($_REQUEST);
  var_dump($_FILES);
  echo "Caption is ".$_POST["caption"]."    ";
  foreach($_FILES as $file){
@@ -12,7 +12,7 @@
  function uploadImage($file,$uploaddir = "images"){
  	$filename = $file['name'];
  	if(move_uploaded_file($file["tmp_name"], "$uploaddir/$filename")){
-    echo "$fileName upload is complete";
+    echo "$filename upload is complete";
 } else {
     echo "move_uploaded_file function failed";
 }
